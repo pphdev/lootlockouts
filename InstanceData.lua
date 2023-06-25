16641cd7551fa0cc259613b082a5ebb44fd757db
pphdev <137341558+pphdev@users.noreply.github.com>
2023-06-25 16:34:10 +0200

    Changed boss name

--- InstanceData.lua
+++ InstanceData.lua
@@ -15,7 +15,7 @@ InstanceData.nhl = {"Rokmora", "Ularogg Cragshaper", "Naraxas", "Dargrul"};
 InstanceData.hov = {"Hymdall", "Hyrja", "Fenryr", "Skovald", "Odyn"};
 InstanceData.vio = {"Festerface", "Shivermaw", "Blood-Princess Thal'ena", "Mindflayer Kaahrj", "\n", "Millificent Manastorm", "Anub'esset", "Sael'orn", "Fel Lord Betrug"}
 
-InstanceData.en = {"Nythendra", "Elerethe Renferal", "Ursoc", "Il'gynoth", "Dragons of Nightmare", "Cenarius", "Xavius"};
+InstanceData.en = {"Nythendra", "Elerethe Renferal", "Ursoc", "Il'gynoth", "Ysondre", "Cenarius", "Xavius"};
 
 InstanceData.instances = {NHL = InstanceData.nhl, DHT = InstanceData.dht, COS = InstanceData.cos, BRH = InstanceData.brh,
                       VOTW = InstanceData.votw, MOS = InstanceData.mos, ARC = InstanceData.arc, HOV = InstanceData.hov, VIO = InstanceData.vio,
