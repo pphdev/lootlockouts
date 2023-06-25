6e8fd0157d92893d9130feb3c493068d6f2b1d2f
pphdev <137341558+pphdev@users.noreply.github.com>
2023-06-25 16:19:58 +0200

    Added support for EN

--- InstanceData.lua
+++ InstanceData.lua
@@ -15,8 +15,11 @@ InstanceData.nhl = {"Rokmora", "Ularogg Cragshaper", "Naraxas", "Dargrul"};
 InstanceData.hov = {"Hymdall", "Hyrja", "Fenryr", "Skovald", "Odyn"};
 InstanceData.vio = {"Festerface", "Shivermaw", "Blood-Princess Thal'ena", "Mindflayer Kaahrj", "\n", "Millificent Manastorm", "Anub'esset", "Sael'orn", "Fel Lord Betrug"}
 
+InstanceData.en = {"Nythendra", "Elerethe Renferal", "Ursoc", "Il'gynoth", "Dragons of Nightmare", "Cenarius", "Xavius"};
 
 InstanceData.instances = {NHL = InstanceData.nhl, DHT = InstanceData.dht, COS = InstanceData.cos, BRH = InstanceData.brh,
                       VOTW = InstanceData.votw, MOS = InstanceData.mos, ARC = InstanceData.arc, HOV = InstanceData.hov, VIO = InstanceData.vio,
                     EOA = InstanceData.eoa};
 
+InstanceData.raids = {["EN (N)"] = InstanceData.en, ["EN (HC)"] = InstanceData.en, ["EN (M)"] = InstanceData.en};
+InstanceData.raidkeys = {"EN (N)", "EN (HC)", "EN (M)"};
